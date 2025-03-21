6631512f1274da41abdc1eea567b94d48aa22507
StellaTeuscher <stella.teuscher@gmail.com>
2025-03-21 10:57:17 -0400

    hidden stuff

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
+import "dotenv/config";
 
 app.get('/', (req, res)=> {
     res.send('The server is running');
 })
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000")
+    console.log("Listening on port 3000");
+    console.log("Your secret is " + process.env.SECRET_PASSWORD);
 })
