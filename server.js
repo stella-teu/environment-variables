33e2ebc030b2e0d08d55899469de768df626a02c
StellaTeuscher <stella.teuscher@gmail.com>
2025-03-21 10:37:50 -0400

    setup

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('The server is running');
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
